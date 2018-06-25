4ef9aba2b3743fc024029b1061871f7d6dea1e88
Rositsa Todinova <rtodinova@uni-sofia.bg>
2018-06-25 08:25:35 +0300

    Class ChatbotRunner included

--- chatbot.py
+++ chatbot.py
@@ -8,25 +8,38 @@ Created on Wed Jun 20 22:32:43 2018
 
 from parsers import JsonParser, ResponseParser
 
+
 class Chatbot:
     
     def __init__(self, responses):
         self.responses = responses
         self.response = ResponseParser()
     
+    
     def respond(self, user_input):
         return self.response.search_answer_in_responses(self.responses, user_input)
-            
-def main():
-    parser = JsonParser()
-    responses = parser.parse_to_dictionary()
-    chatbot_runner = Chatbot(responses)
     
-    user_message = input("USER: ")
-    while user_message != "exit":
-        print(chatbot_runner.respond(user_message))
-        user_message = input("USER: ")
+    
+class ChatbotRunner:
+    
+    def __init__(self):
+        self.parser = JsonParser()
+        responses = self.parser.parse_to_dictionary()
+        self.chatbot = Chatbot(responses)
         
-    parser.parse_to_json(chatbot_runner.responses)
+        
+    def run(self):
+        user_message = input("USER: ")
+        while user_message != "exit":
+            print(self.chatbot.respond(user_message))
+            user_message = input("USER: ")
+        
+        self.parser.parse_to_json(self.chatbot.responses)
+
+          
+def main():
+    chatbot = ChatbotRunner()
+    chatbot.run()
+    
         
 if __name__ == "__main__": main()
