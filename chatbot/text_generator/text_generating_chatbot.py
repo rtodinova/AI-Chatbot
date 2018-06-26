a704968b38973d855c36e9d6dc64af8b38cd4617
Rositsa Todinova <rtodinova@uni-sofia.bg>
2018-06-27 00:23:29 +0300

    Class TextGeneratingChatbot added

--- chatbot/text_generator/text_generating_chatbot.py
+++ chatbot/text_generator/text_generating_chatbot.py
@@ -5,3 +5,10 @@ Created on Wed Jun 27 00:00:31 2018
 @author: RTodinova
 """
 
+
+from ..chatbot import Chatbot
+from statement import Statement
+
+class TextGeneratingChatbot(Chatbot):
+    def __init__(self):
+        self.word_sequences = {}
