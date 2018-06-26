8917d3b5237974c751dc047a220c6fdf712680fc
Rositsa Todinova <rtodinova@uni-sofia.bg>
2018-06-27 00:16:48 +0300

    Class Statement added

--- chatbot/text_generator/statement.py
+++ chatbot/text_generator/statement.py
@@ -5,3 +5,7 @@ Created on Wed Jun 27 00:11:53 2018
 @author: RTodinova
 """
 
+class Statement:
+    def __init__(self, word, frequency):
+        self.word = word
+        self.frequency = frequency
